8b3de2106ec103151599f9189d69ea30690123f9
josedomingues94 <josedomingues2894@gmail.com>
2025-12-26 15:32:14 +0100

    feat: add carrousel

--- js/main.js
+++ js/main.js
@@ -23,3 +23,25 @@ navLinks.forEach(link => {
 
 // Cerrar al tocar fuera
 overlay.addEventListener("click", closeMenu);
+
+const carousel = document.querySelector(".carousel");
+const track = carousel.querySelector(".carousel-track");
+const slides = Array.from(track.children);
+const nextBtn = carousel.querySelector(".carousel-btn.next");
+const prevBtn = carousel.querySelector(".carousel-btn.prev");
+
+let index = 0;
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  index = (index + 1) % slides.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  index = (index - 1 + slides.length) % slides.length;
+  updateCarousel();
+});
